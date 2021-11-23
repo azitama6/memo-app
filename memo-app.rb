dc2baafbc51bc2033b3ad38064ddb5ea5097d6b0
yusuke.nakazato <ajitama6@gmail.com>
2021-11-23 16:36:29 +0900

    add editMemo with erb

--- memo-app.rb
+++ memo-app.rb
@@ -7,7 +7,6 @@ get '/top' do
     memos = JSON.parse(file.read)
     @h += '<ul>'
     memos['memos'].each do |data|
-      puts data['id']
       @h += "<li><a href='http://localhost:4567/#{data['id']}/show-memo'>#{data['title']}</a></li>"
     end
     @h += '</ul>'
@@ -24,11 +23,42 @@ get '/:id/show-memo' do
     find_data = memos['memos'].find { |item| item['id'] == params[:id].to_s }
     p find_data
     if find_data != ''
+      @id = find_data['id']
       @title = find_data['title']
       @body = find_data['body']
     end
   end
   erb :showMemo
+end
+
+post '/editMemo/:id' do
+  @title = ''
+  @body = ''
+  p params[:id].to_s
+  File.open('data.json', 'r') do |file|
+    memos = JSON.parse(file.read)
+    find_data = memos['memos'].find { |item| item['id'] == params[:id].to_s }
+    p find_data
+    if find_data != ''
+      @id = find_data['id']
+      @title = find_data['title']
+      @body = find_data['body']
+    end
+  end
+  erb :editMemo
+end
+
+post '/editMemo/:id/update' do
+  File.open('data.json', 'r+') do |file|
+    memos = JSON.parse(file.read)
+    p result = memos['memos'].map { |item| item['id'] == params[:id].to_s ? params[:title] : item['title'] }
+    # mods.map! { |item| item['id'] == params[:id].to_s ? params[:body] : item['body'] }
+    # p memos['memos']
+    # if memos['memos']
+    #  file.write(memos['memos'])
+    # end
+  end
+  redirect '/top'
 end
 
 get '/page-notfound' do
