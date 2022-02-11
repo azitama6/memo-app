fb6215b3d45a48aa79f51ef5c43529f5af0a2bfc
yusuke.nakazato <ajitama6@gmail.com>
2022-02-11 18:38:03 +0900

    DBアクセス用のMemoクラス作成、DBアクセスメソッド作成

--- memo_app.rb
+++ memo_app.rb
@@ -2,6 +2,41 @@
 
 require 'sinatra'
 require 'json'
+require 'pg'
+require 'dotenv'
+
+Dotenv.load
+
+# 環境変数確認
+p ENV["DB_HOST"]
+p ENV["DB_PASS"]
+p ENV["DB_USER"]
+p ENV["DB_NAME"]
+p ENV["DB_PORT"]
+p ENV["TABLE_NAME"]
+
+class Memo
+  class << self
+    def find_memo(connection, id)
+      memos = connection.exec("SELECT * FROM #{ENV["TABLE_NAME"]} WHERE id = #{id} ").map{|row| row }
+      memos.find { |memo| memo['id'] == id.to_s }
+    end
+
+    def create_memo(connection, title, body)
+      connection.exec("INSERT INTO #{ENV["TABLE_NAME"]} (title, body) VALUES(#{title}, #{body})")
+    end
+
+    def delete_memo(connection, id)
+      connection.exec("DELETE FROM #{ENV["TABLE_NAME"]} WHERE id = #{id} ")
+    end
+
+    def update_memo(connection, id, title, body)
+      connection.exec("UPDATE #{ENV["TABLE_NAME"]} SET title = #{title}, body = #{body} WHERE id = #{id} ")
+    end
+  end
+end
+
+connection = PG::connect(host: ENV["DB_HOST"], password: ENV["DB_PASS"], user: ENV["DB_USER"], dbname: ENV["DB_NAME"], port: ENV["DB_PORT"])
 
 get '/' do
   @memos = JSON.parse(File.read('data.json'))
@@ -27,7 +62,7 @@ end
 get '/memos/:id' do
   @title = ''
   @body = ''
-  find_data = find_memo(params[:id])
+  find_data = Memo.find_memo(connection, params[:id])
   @memo = find_data unless find_data.empty?
   erb :showMemo
 end
@@ -35,7 +70,7 @@ end
 get '/editMemo/:id' do
   @title = ''
   @body = ''
-  find_data = find_memo(params[:id])
+  find_data = Memo.find_memo(connection, params[:id])
   @memo = find_data unless find_data.empty?
   erb :editMemo
 end
@@ -76,9 +111,4 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
-end
-
-def find_memo(id)
-  memos = JSON.parse(File.read('data.json'))
-  memos['memos'].find { |memo| memo['id'] == id.to_s }
 end
