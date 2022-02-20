029052ae7c74dabdcf10b8c9fde78b899898a16b
yusuke.nakazato <ajitama6@gmail.com>
2022-02-20 12:19:21 +0900

    DBアクセスするクラスとメソッドを作成

--- memo_app.rb
+++ memo_app.rb
@@ -7,23 +7,18 @@ require 'dotenv'
 
 Dotenv.load
 
-# 環境変数確認
-p ENV["DB_HOST"]
-p ENV["DB_PASS"]
-p ENV["DB_USER"]
-p ENV["DB_NAME"]
-p ENV["DB_PORT"]
-p ENV["TABLE_NAME"]
-
 class Memo
   class << self
-    def find_memo(connection, id)
+    def all_memos(connection)
+      p connection.exec("SELECT * FROM #{ENV["TABLE_NAME"]} ").map{|row| row }
+    end
+    def find_memos(connection, id)
       memos = connection.exec("SELECT * FROM #{ENV["TABLE_NAME"]} WHERE id = #{id} ").map{|row| row }
       memos.find { |memo| memo['id'] == id.to_s }
     end
 
     def create_memo(connection, title, body)
-      connection.exec("INSERT INTO #{ENV["TABLE_NAME"]} (title, body) VALUES(#{title}, #{body})")
+      connection.exec("INSERT INTO #{ENV["TABLE_NAME"]} (title, body) VALUES(\'#{title}\', \'#{body}\') ")
     end
 
     def delete_memo(connection, id)
@@ -31,7 +26,7 @@ class Memo
     end
 
     def update_memo(connection, id, title, body)
-      connection.exec("UPDATE #{ENV["TABLE_NAME"]} SET title = #{title}, body = #{body} WHERE id = #{id} ")
+      connection.exec("UPDATE #{ENV["TABLE_NAME"]} SET title = \'#{title}\', body = \'#{body}\' WHERE id = #{id} ")
     end
   end
 end
@@ -39,7 +34,7 @@ end
 connection = PG::connect(host: ENV["DB_HOST"], password: ENV["DB_PASS"], user: ENV["DB_USER"], dbname: ENV["DB_NAME"], port: ENV["DB_PORT"])
 
 get '/' do
-  @memos = JSON.parse(File.read('data.json'))
+  @memos = Memo.all_memos(connection)
   erb :index
 end
 
@@ -48,54 +43,29 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  max_id = 0
-  memos = JSON.parse(File.read('data.json'))
-  memos['memos'].each do |memo|
-    max_id = memo['id'].to_i if max_id < memo['id'].to_i
-  end
-  new_data = { "id": (max_id + 1).to_s, "title": params[:title], "body": params[:body] }
-  memos['memos'].push(new_data)
-  File.write('data.json', memos.to_json)
+  Memo.create_memo(params[:title], params[:body])
   redirect '/'
 end
 
 get '/memos/:id' do
-  @title = ''
-  @body = ''
-  find_data = Memo.find_memo(connection, params[:id])
+  find_data = Memo.find_memos(connection, params[:id])
   @memo = find_data unless find_data.empty?
   erb :showMemo
 end
 
 get '/editMemo/:id' do
-  @title = ''
-  @body = ''
-  find_data = Memo.find_memo(connection, params[:id])
+  find_data = Memo.find_memos(connection, params[:id])
   @memo = find_data unless find_data.empty?
   erb :editMemo
 end
 
 patch '/editMemo/:id/update' do
-  memos = JSON.parse(File.read('data.json'))
-  memos['memos'].each do |memo|
-    if memo['id'] == params[:id]
-      memo['title'] = params[:title]
-      memo['body'] = params[:body]
-    end
-  end
-  File.write('data.json', memos.to_json)
+  Memo.update_memo(connection, params[:id], params[:title], params[:body])
   redirect '/'
 end
 
 delete '/delete/:id' do
-  new_hash = {}
-  new_array = []
-  memos = JSON.parse(File.read('data.json'))
-  memos['memos'].each do |memo|
-    new_array.push(memo) if memo['id'] != params[:id]
-    new_hash = { 'memos': new_array }
-  end
-  File.write('data.json', new_hash.to_json)
+  Memo.delete_memo(connection, params[:id])
   redirect '/'
 end
 
