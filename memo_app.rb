984ff0561fa957e50d988297bfa90715d343c8e0
yusuke.nakazato <ajitama6@gmail.com>
2022-05-08 12:09:04 +0900

    Memoクラスのメソッド名修正、SQL修正、不要なmapを削除

--- memo_app.rb
+++ memo_app.rb
@@ -9,24 +9,24 @@ Dotenv.load
 
 class Memo
   class << self
-    def all_memos(connection)
-      connection.exec("SELECT * FROM #{ENV["TABLE_NAME"]} ").map{|row| row }
+    def all_memo(connection)
+      connection.exec("SELECT * FROM MEMO ORDER BY ID")
     end
-    def find_memos(connection, id)
-      memos = connection.exec("SELECT * FROM #{ENV["TABLE_NAME"]} WHERE id = #{id} ").map{|row| row }
+    def find_memo(connection, id)
+      memos = connection.exec("SELECT * FROM MEMO WHERE id = #{id} ORDER BY ID")
       memos.find { |memo| memo['id'] == id.to_s }
     end
 
     def create_memo(connection, title, body)
-      connection.exec("INSERT INTO #{ENV["TABLE_NAME"]} (title, body) VALUES(\'#{title}\', \'#{body}\') ")
+      connection.exec("INSERT INTO MEMO (title, body) VALUES(\'#{title}\', \'#{body}\') ")
     end
 
     def delete_memo(connection, id)
-      connection.exec("DELETE FROM #{ENV["TABLE_NAME"]} WHERE id = #{id} ")
+      connection.exec("DELETE FROM MEMO WHERE id = #{id} ")
     end
 
     def update_memo(connection, id, title, body)
-      connection.exec("UPDATE #{ENV["TABLE_NAME"]} SET title = \'#{title}\', body = \'#{body}\' WHERE id = #{id} ")
+      connection.exec("UPDATE MEMO SET title = \'#{title}\', body = \'#{body}\' WHERE id = #{id} ")
     end
   end
 end
@@ -34,7 +34,7 @@ end
 connection = PG::connect(host: ENV["DB_HOST"], password: ENV["DB_PASS"], user: ENV["DB_USER"], dbname: ENV["DB_NAME"], port: ENV["DB_PORT"])
 
 get '/' do
-  @memos = Memo.all_memos(connection)
+  @memos = Memo.all_memo(connection)
   erb :index
 end
 
@@ -48,7 +48,7 @@ post '/memos' do
 end
 
 get '/memos/:id' do
-  find_data = Memo.find_memos(connection, params[:id])
+  find_data = Memo.find_memo(connection, params[:id])
   @memo = find_data unless find_data.empty?
   erb :showMemo
 end
